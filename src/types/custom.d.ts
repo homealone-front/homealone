0c41d8b3e2e71b3390d90943eceb4d1267585a64
an donghyun <ax34554@gmail.com>
2024-05-15 20:36:06 +0900

    chore: 프로젝트 초기 세팅

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,9 +1,2 @@
 type PropType<T, K extends keyof T> = T[K];
 type ArrayType<P extends Array> = P extends Array<infer T> ? T : unknown;
-
-/**
- * 빈 문자열 리터럴 타입 추가
- */
-type WithEmptyString<T> = {
-  [K in keyof T]: T[K] extends object ? WithEmptyString<T[K]> : T[K] | '';
-};
