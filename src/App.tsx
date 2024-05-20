24a7f523ee5049d89323adfa1740d026039cc0d4
an donghyun <ax34554@gmail.com>
2024-05-20 18:24:38 +0900

    chore: tailwind, shadcu/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 // import React from 'react';
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
     <>
-      <h1>Project First Setting</h1>
+      <h1 className="text-red400">클릭해보세요</h1>
+      <Button>컨셉컬러세팅</Button>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import '@/lib/tailwind.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
