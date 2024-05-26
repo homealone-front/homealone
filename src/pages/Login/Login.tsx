28e01d62009b5a99520661a9406e3e5443891c32
an donghyun <ax34554@gmail.com>
2024-05-26 15:08:24 +0900

    feat: 로그인페이지 (api제외)

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,18 +1,22 @@
-import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+import { Layout } from '@/layout';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/Input';
+import { Appbar } from '@/components/Appbar';
 
 import { PATH } from '@/constants/paths';
+
+import { usePageMoveHandler } from '@/hooks/usePageMoveHandler';
 
 import { MemberLoginFetchParams } from '@/api/member/memberLoginPostFetch';
 
 import { loginSchema } from './validator';
 
 const Login = () => {
-  const navigate = useNavigate();
+  const navigate = usePageMoveHandler();
 
   const method = useForm({
     resolver: yupResolver(loginSchema),
@@ -25,7 +29,7 @@ const Login = () => {
   const {
     handleSubmit: submit,
     control,
-    // getValues,
+    getValues,
     // setValue,
     // trigger,
     // watch,
@@ -40,38 +44,56 @@ const Login = () => {
     }
   });
 
+  console.info('파라미터 확인', getValues());
+
   return (
-    <div className="w-[30rem] m-auto pt-10">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Input<MemberLoginFetchParams>
-            control={control}
-            name="email"
-            type="text"
-            extractNumber={false}
-            label="Email"
-            placeholder="example@example.com"
-            error={errors?.email}
-          />
-        </div>
+    <>
+      <Appbar />
+      <Layout>
+        <div className="w-[30rem] m-auto pt-10">
+          <div className="mb-14">
+            <h3 className="text-2xl text-center text-primary font-semibold mb-6">로그인</h3>
+            <p className="text-lg text-center text-gray400">이메일로 로그인</p>
+          </div>
 
-        <div className="mt-2 mb-4">
-          <Input<MemberLoginFetchParams>
-            control={control}
-            name="password"
-            type="password"
-            extractNumber={false}
-            label="Password"
-            placeholder="영어 대소문자, 특수문자 1자를 포함"
-            error={errors?.password}
-          />
-        </div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <Input
+                control={control}
+                name="email"
+                type="text"
+                extractNumber={true}
+                label="이메일"
+                placeholder="example@example.com"
+                error={errors?.email}
+              />
+            </div>
 
-        <Button className="w-full" type="submit">
-          로그인
-        </Button>
-      </form>
-    </div>
+            <div className="my-8">
+              <Input
+                control={control}
+                name="password"
+                type="password"
+                extractNumber={false}
+                label="비밀번호"
+                placeholder="영어 대소문자, 특수문자 1자를 포함"
+                error={errors?.password}
+              />
+            </div>
+
+            <Button className="w-full" type="submit">
+              로그인
+            </Button>
+          </form>
+          <div className="text-center mt-8 font-light">
+            아직 회원이 아니신가요?{' '}
+            <span onClick={() => navigate(PATH.register)} className="ml-2 underline text-blue500 cursor-pointer">
+              회원가입
+            </span>
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 };
 
