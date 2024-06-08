6e761db0804063efb80921313560b6c9ddd099d7
LEE SUL <suerish.e@gmail.com>
2024-06-08 20:35:24 +0900

    style: 방자랑 yup 스키마 변경

--- src/pages/RoomWrite/validator.ts
+++ src/pages/RoomWrite/validator.ts
@@ -2,16 +2,16 @@ import * as yup from 'yup';
 
 // yup 스키마 정의
 export const roomSchema = yup.object().shape({
-  title: yup.string().required('방자랑 제목을 입력해 주세요'),
-  content: yup.string().required('내용을 입력해 주세요'),
+  title: yup.string().required('방자랑 제목을 입력해 주세요!'),
+  content: yup.string().required('방자랑 내용을 입력해 주세요!'),
   thumbnailUrl: yup.array().of(
     yup.object().shape({
       image: yup
         .mixed()
-        .required('대표이미지를 선택해주세요!')
+        .required('대표이미지를 선택해 주세요!')
         .test('is-file', '대표이미지는 파일이어야 합니다!', (value) => value instanceof File),
 
-      imageUrl: yup.string().required('대표이미지를 선택해주세요!'),
+      imageUrl: yup.string().required('대표이미지를 선택해 주세요!'),
     }),
   ),
   roomImages: yup.array().of(yup.string()),
