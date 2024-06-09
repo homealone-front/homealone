aa76b9e3938a48ed84c6a6d32768532d33479e35
[SW5] 이수민 <tnklr1111@gmail.com>
2024-06-10 02:53:20 +0900

    fix: 하트/북마크 애니메이션 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -175,7 +175,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'button-jump': 'button-jump 0.5s  ease-out',
+        'button-jump': 'button-jump 0.2s  ease-out',
       },
     },
   },
