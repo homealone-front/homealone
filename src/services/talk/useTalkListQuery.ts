1776180828a58875dfd59bddd10f68667333ac35
[SW5] 이수민 <tnklr1111@gmail.com>
2024-06-10 18:28:25 +0900

    feat: 혼잣말 목록 조회 useQuery 적용

---
+++ src/services/talk/useTalkListQuery.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { talkListGetFetch, TalkListGetFetchParms } from '@/api/talk/talkListGetFetch';
+
+/**
+ * 혼잣말 전체 조회
+ */
+
+export const useTalkListQuery = (params: TalkListGetFetchParms) =>
+  useQuery({
+    queryKey: ['@talkList', params],
+    queryFn: async () => {
+      const response = await talkListGetFetch(params);
+
+      const { data } = response;
+
+      console.info(data);
+
+      const { totalElements, totalPages, pageable, content } = data;
+
+      return {
+        totalElements,
+        totalPages,
+        pageable,
+        content,
+      };
+    },
+    staleTime: 5000,
+  });
