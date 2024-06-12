9626873d42acd1c26fb814ee82598417e1a7d548
LEE SUL <suerish.e@gmail.com>
2024-06-13 01:28:06 +0900

    fix: 방자랑 상세 불러올때 isFetching 조건부 적용

--- src/pages/RoomDetail/RoomDetail.tsx
+++ src/pages/RoomDetail/RoomDetail.tsx
@@ -21,6 +21,7 @@ import { CommentForm } from '@/components/CommentForm';
 import { SkeletonComment } from '@/components/SkeletonComment';
 import { Comment } from '@/components/Comment';
 import { useUserStore } from '@/store/useUserStore';
+import { Spinner } from '@/components/Spinner';
 
 /**
  * 방자랑 게시글 상세페이지
@@ -35,7 +36,7 @@ const RoomDetail = () => {
   // console.info('🚀 ~ RoomDetail ~ roomId:', roomId);
   if (!roomId) return;
 
-  const { data } = useRoomDetailQuery({ roomId });
+  const { data, isFetching } = useRoomDetailQuery({ roomId });
   console.info('🚀 ~ RoomDetail ~ data:', data);
 
   const {
@@ -85,82 +86,87 @@ const RoomDetail = () => {
   return (
     <>
       <Appbar />
-
       <Layout>
-        <Marks
-          onLikesSubmit={() => alert('좋아요 반영 핸들러')}
-          onBookmarkSubmit={() => alert('북마크 반영 핸들러')}
-          likes={40}
-          isLike={true}
-          isBookmark={true}
-        />
-        <div className="w-3/4 mx-auto flex flex-col gap-8 pb-8">
-          {/* TODO: 작성자 id값 조건주기 */}
-          <section className="border-b pb-4 flex justify-between items-center">
-            <h3 className="text-3xl font-semibold">{data?.title}</h3>
-            <div className="flex gap-4">
-              <Button variant="secondary">수정</Button>
-              <Button variant="secondary">삭제</Button>
-            </div>
-          </section>
-          <section className="flex items-center justify-between">
-            <div className="flex gap-2 items-center text-sm">
-              <Avatar>
-                <AvatarImage src={''} />
-                <AvatarFallback>{data?.memberName}</AvatarFallback>
-              </Avatar>
-              By <span className="text-lg">{data?.memberName}</span>
-            </div>
-            <span className="text-gray500 text-sm">{dayjs(data?.createdAt).format('YYYY년 MM월 DD일')}</span>
-          </section>
+        {!isFetching ? (
+          <>
+            <Marks
+              onLikesSubmit={() => alert('좋아요 반영 핸들러')}
+              onBookmarkSubmit={() => alert('북마크 반영 핸들러')}
+              likes={40}
+              isLike={true}
+              isBookmark={true}
+            />
+            <div className="w-3/4 mx-auto flex flex-col gap-8 pb-8">
+              {/* TODO: 작성자 id값 조건주기 */}
+              <section className="border-b pb-4 flex justify-between items-center">
+                <h3 className="text-3xl font-semibold">{data?.title}</h3>
+                <div className="flex gap-4">
+                  <Button variant="secondary">수정</Button>
+                  <Button variant="secondary">삭제</Button>
+                </div>
+              </section>
+              <section className="flex items-center justify-between">
+                <div className="flex gap-2 items-center text-sm">
+                  <Avatar>
+                    <AvatarImage src={''} />
+                    <AvatarFallback>{data?.memberName}</AvatarFallback>
+                  </Avatar>
+                  By <span className="text-lg">{data?.memberName}</span>
+                </div>
+                <span className="text-gray500 text-sm">{dayjs(data?.createdAt).format('YYYY년 MM월 DD일')}</span>
+              </section>
 
-          <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+              <EmblaCarousel slides={SLIDES} options={OPTIONS} />
 
-          <div className="no-tailwind">{parse(`${data?.content}`)}</div>
+              {data && <div className="no-tailwind">{parse(`${data.content}`)}</div>}
 
-          <div className="flex gap-2 items-center justify-end text-gray500 text-sm border-t pt-5">
-            <div className="flex items-center gap-1">
-              <Eye strokeWidth="1.5" />
-              <span>조회수</span>
-              <span>{data?.view}</span>
-            </div>
-            <span>·</span>
-            <div className="flex items-center gap-1">
-              <MessageSquareMore strokeWidth="1.5" />
-              <span>댓글</span>
-              <span>{data?.commentCount}</span>
-            </div>
-          </div>
-
-          <CommentForm
-            name="content"
-            control={control}
-            imageUrl={''}
-            error={errors?.content}
-            onSubmit={handleSubmit}
-            value={watch('content')}
-          />
-          {commentFetching ? (
-            <div className="flex flex-col justify-center gap-2">
-              {Array.from({ length: 8 }).map((_, i) => (
-                <SkeletonComment key={i} />
-              ))}
-            </div>
-          ) : commentData && commentData.length > 0 ? (
-            commentData?.map((item) => (
-              <Comment key={item.id} write={userId === item?.memberId} commentRefetch={commentRefetch} {...item} />
-            ))
-          ) : (
-            <div className="min-h-40 flex items-center justify-around p-4 border border-gray-300 shadow-md rounded-xl">
-              <div>
-                <p className="leading-8  text-lg text-primary font-semibold">
-                  아직 댓글이 없는 게시글이에요. <br />첫 댓글의 주인공이 되어보세요!
-                </p>
+              <div className="flex gap-2 items-center justify-end text-gray500 text-sm border-t pt-5">
+                <div className="flex items-center gap-1">
+                  <Eye strokeWidth="1.5" />
+                  <span>조회수</span>
+                  <span>{data?.view}</span>
+                </div>
+                <span>·</span>
+                <div className="flex items-center gap-1">
+                  <MessageSquareMore strokeWidth="1.5" />
+                  <span>댓글</span>
+                  <span>{data?.commentCount}</span>
+                </div>
               </div>
-              <img className="w-32 h-32" src="/icons/notFound.svg" alt="" />
+
+              <CommentForm
+                name="content"
+                control={control}
+                imageUrl={''}
+                error={errors?.content}
+                onSubmit={handleSubmit}
+                value={watch('content')}
+              />
+              {commentFetching ? (
+                <div className="flex flex-col justify-center gap-2">
+                  {Array.from({ length: 8 }).map((_, i) => (
+                    <SkeletonComment key={i} />
+                  ))}
+                </div>
+              ) : commentData && commentData.length > 0 ? (
+                commentData?.map((item) => (
+                  <Comment key={item.id} write={userId === item?.memberId} commentRefetch={commentRefetch} {...item} />
+                ))
+              ) : (
+                <div className="min-h-40 flex items-center justify-around p-4 border border-gray-300 shadow-md rounded-xl">
+                  <div>
+                    <p className="leading-8  text-lg text-primary font-semibold">
+                      아직 댓글이 없는 게시글이에요. <br />첫 댓글의 주인공이 되어보세요!
+                    </p>
+                  </div>
+                  <img className="w-32 h-32" src="/icons/notFound.svg" alt="" />
+                </div>
+              )}
             </div>
-          )}
-        </div>
+          </>
+        ) : (
+          <Spinner>방자랑을 불러오고 있어요 ... </Spinner>
+        )}
       </Layout>
     </>
   );
