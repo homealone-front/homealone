4b6d7d86a4192fe1da78bc4c4efcbbaa801510ca
[SW5] 문수민 <sum95771@gmail.com>
2024-06-13 11:57:33 +0900

    feat: 작성한글페이지 레시피리스트컴포넌트

---
+++ src/pages/MyPosts/components/RecipeList/RecipeList.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { Card } from '@/components/Card';
+
+import { PriceSlot } from '@/pages/Main/components/PriceSlot';
+import { Pagination } from '@/components/Pagination';
+
+import { useNavigate, generatePath } from 'react-router-dom';
+import { RECIEPE_PATH } from '@/constants/paths';
+import { useMyRecipeListQuery } from '@/services/recipe/useMyRecipeListQuery';
+import { SkeletonCard } from '@/components/Skeleton';
+import { NoContents } from '../NoContents';
+import { NAV_TABS } from '../../constants';
+
+/**
+ * 내가 작성한 레시피 글 목록 컴포넌트
+ */
+
+const RecipeList = () => {
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const { data, isLoading, isFetching } = useMyRecipeListQuery({ page: currentPage, size: 20 });
+
+  const navigate = useNavigate();
+
+  const handlePageMove = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <>
+      {!data?.content.length ? (
+        <NoContents tab={NAV_TABS.recipe} />
+      ) : (
+        <>
+          <div className="grid grid-cols-4 gap-6 place-items-start py-12">
+            {isLoading || isFetching
+              ? Array.from({ length: 20 }).map((_, index) => <SkeletonCard key={index} />)
+              : data?.content?.map((card, i) => (
+                  <Card
+                    key={i}
+                    title={card?.title}
+                    description={card?.description}
+                    userName={card?.userName}
+                    imageUrl={card?.imageUrl}
+                    lineClamp={1}
+                    slot={
+                      <PriceSlot
+                        cookInfo={{
+                          portions: card?.portions,
+                          cookTime: card?.recipeTime,
+                        }}
+                      />
+                    }
+                    likes={40}
+                    onPageMove={() =>
+                      navigate(
+                        generatePath(RECIEPE_PATH.detail, {
+                          id: card.id.toString(),
+                        }),
+                      )
+                    }
+                  />
+                ))}
+          </div>
+          <Pagination totalPage={data?.totalPages as number} currentPage={currentPage} onPageChange={handlePageMove} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default RecipeList;
