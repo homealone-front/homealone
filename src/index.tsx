e3307c8574fabcaccc8191a32f55f346b99ba0ff
LEE SUL <suerish.e@gmail.com>
2024-06-13 21:01:25 +0900

    chore: @tanstack/react-query-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,13 @@ import App from './App';
 
 import { queryClient } from './services/quries';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
