842fb66b566cba30d5e7cd80a96e1438a56f00e7
an donghyun <ax34554@gmail.com>
2024-06-14 21:48:27 +0900

    style: 주석처리

--- src/pages/Chattings/Chattings.tsx
+++ src/pages/Chattings/Chattings.tsx
@@ -1,12 +1,12 @@
-import {
-  useNavigate,
-  //  generatePath
-} from 'react-router-dom';
-// import axios from "axios";
-import {
-  // useState,
-  useEffect,
-} from 'react';
+// import {
+//   useNavigate,
+//   //  generatePath
+// } from 'react-router-dom';
+// // import axios from "axios";
+// import {
+//   // useState,
+//   useEffect,
+// } from 'react';
 // import {jwtDecode} from "jwt-decode";
 
 import { Appbar } from '@/components/Appbar';
@@ -14,23 +14,23 @@ import { Footer } from '@/components/Footer';
 import { Layout } from '@/layout';
 
 const Chattings = () => {
-  const navigate = useNavigate();
+  //   const navigate = useNavigate();
   // const [member, setMember] = useState([]);
   // const [chatroomList, setChatroomList] = useState([]);
 
-  useEffect(() => {
-    if (localStorage.getItem('accessToken') === null) {
-      if (window.confirm('로그인이 필요합니다. \n로그인 하시겠습니까?')) {
-        navigate('/login');
-      } else {
-        navigate('/');
-        console.info('현재 로그인 상태');
-      }
-    } else {
-      // const token = localStorage.getItem('accessToken');
-      /*accessToken 가져와서 현재 로그인한 사용자의 memberId를 통해 서버에서 데이터 가져오기*/
-    }
-  });
+  //   useEffect(() => {
+  //     if (localStorage.getItem('accessToken') === null) {
+  //       if (window.confirm('로그인이 필요합니다. \n로그인 하시겠습니까?')) {
+  //         navigate('/login');
+  //       } else {
+  //         navigate('/');
+  //         console.info('현재 로그인 상태');
+  //       }
+  //     } else {
+  //       // const token = localStorage.getItem('accessToken');
+  //       /*accessToken 가져와서 현재 로그인한 사용자의 memberId를 통해 서버에서 데이터 가져오기*/
+  //     }
+  //   });
 
   return (
     <>
