31212a972fee6d72025376b252c251f148d1fdcd
[SW5] 이수민 <tnklr1111@gmail.com>
2024-06-14 11:21:39 +0900

    fix: 커스텀 카드 푸터 좋아요 렌더링 조건 변경

--- src/pages/Talk/Talk.tsx
+++ src/pages/Talk/Talk.tsx
@@ -71,7 +71,7 @@ const Talk = () => {
                   title={card?.title}
                   userName={card?.memberName}
                   lineClamp={1}
-                  userImageUrl={card?.imageUrl}
+                  userImage={card?.imageUrl}
                   slot={<RoomCardSlot createdAt={card?.createdAt} commentCount={card?.commentCount} />}
                   likes={card?.likeCount}
                   onPageMove={() =>
