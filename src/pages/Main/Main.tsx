649be0df52e1380c1b9594d54e4bbe5f75c4e96d
SUMINMOON <sum95771@gmail.com>
2024-11-11 23:58:38 +0900

    style: 인기 혼잣말  리스트 반응형 적용

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -100,7 +100,7 @@ const Main = () => {
         description="혼잣말은 일상생활에 힘이 됩니다."
         onPageMove={() => navigate(PATH.talk)}
       />
-      <div className="grid grid-cols-4 gap-6 mb-20 place-items-start">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-20 place-items-start">
         {talkIsLoading || talkIsFetching
           ? Array.from({ length: talkData?.size as number }).map((_, index) => <SkeletonCard key={index} />)
           : talkData?.content?.map((card) => (
