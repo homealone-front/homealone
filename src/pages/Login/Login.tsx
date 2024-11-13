8f6a0ac19a2f5aed4ed6d852b72f6aba6283eae4
SUMINMOON <sum95771@gmail.com>
2024-11-13 22:12:53 +0900

    feat: handleGoogleLogin 추가 및 스타일 변경

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -20,10 +20,11 @@ import { CircleCheck, CircleXIcon } from 'lucide-react';
 
 import { memberInfoGetFetch } from '@/api/member/memberInfoGetFetch';
 
-import { KakaoButton } from '@/components/KakaoButton';
+import { SocialLoginButton } from '@/components/SocialLoginButton';
 import { KakaoAuthUrlGetFetch } from '@/api/member/kakaoAuthUrlGetFetch';
 import { naverAuthUrlGetFetch } from '@/api/member/naverAuthUrlGetFetch';
 import dayjs from 'dayjs';
+import { googleAuthUrlGetFetch } from '@/api/member/googleAuthUrlGetFetch';
 
 const Login = () => {
   const navigate = usePageMoveHandler();
@@ -126,17 +127,37 @@ const Login = () => {
     }
   };
 
+  const handleGoogleLogin = async () => {
+    try {
+      const urlResponse = await googleAuthUrlGetFetch();
+
+      const naverUrl = document.createElement('a');
+
+      naverUrl.setAttribute('href', urlResponse.data);
+      naverUrl.click();
+    } catch (error) {
+      console.error(error);
+
+      if (isAxiosError(error)) {
+        toast({
+          title: error?.response?.data.message || '구글 로그인 실패',
+          icon: <CircleXIcon />,
+          className: TOAST.error,
+        });
+      }
+    }
+  };
   // console.info('파라미터 확인', getValues());
 
   return (
     <>
-      <div className="w-[30rem] m-auto pt-10">
-        <div className="mb-14">
-          <h3 className="mb-6 text-2xl font-semibold text-center text-primary">로그인</h3>
+      <div className="w-[30rem] m-auto pt-4">
+        <div className="mb-8">
+          <h3 className="mb-4 text-2xl font-semibold text-center text-primary">로그인</h3>
           <p className="text-lg text-center text-gray400">이메일로 로그인</p>
         </div>
 
-        <div>
+        <div className="flex flex-col gap-2">
           <Input
             control={control}
             name="email"
@@ -145,9 +166,7 @@ const Login = () => {
             placeholder="example@example.com"
             error={errors?.email}
           />
-        </div>
 
-        <div className="my-8">
           <Input
             control={control}
             name="password"
@@ -156,19 +175,29 @@ const Login = () => {
             placeholder="영어 대소문자, 특수문자 1자를 포함"
             error={errors?.password}
           />
+
+          <Button className="w-full mt-4" onClick={handleSubmit}>
+            로그인
+          </Button>
         </div>
 
-        <Button className="w-full mb-2" onClick={handleSubmit}>
-          로그인
-        </Button>
-        <KakaoButton buttonText="카카오로 시작하기" onSubmit={handleKakaoLogin} />
-        <Button
-          className="flex items-center w-full gap-6 mt-2 text-white bg-green-500 hover:bg-green-600 active:bg-green-700"
-          onClick={handleNaverLogin}
-        >
-          <img className="h-full" src="/icons/naverlogo.png" alt="naverlogo" />
-          네이버로 시작하기
-        </Button>
+        <div className="w-full h-[1px] bg-gray-200 mt-8"></div>
+        <p className="text-sm text-center text-gray400 my-4">또는</p>
+
+        <div className="flex flex-col gap-2">
+          <SocialLoginButton onSubmit={handleKakaoLogin} variant="kakao">
+            <img src={'/icons/kakaologo.svg'} alt={'naver-logo'} />
+            카카오로 시작하기
+          </SocialLoginButton>
+          <SocialLoginButton onSubmit={handleNaverLogin} variant="naver">
+            <img className="h-full" src={'/icons/naverlogo.png'} alt={'naver-logo'} />
+            네이버로 시작하기
+          </SocialLoginButton>
+          <SocialLoginButton onSubmit={handleGoogleLogin} variant="google">
+            <img src={'/icons/google_logo.svg'} alt={'google-logo'} />
+            구글로 시작하기
+          </SocialLoginButton>
+        </div>
 
         <div className="mt-8 font-light text-center">
           아직 회원이 아니신가요?{' '}
