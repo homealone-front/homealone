2484885b3d20cd121ead574c78e9078c28787b0d
SUMINMOON <sum95771@gmail.com>
2024-11-15 12:35:51 +0900

    style: ListTitle 더보기 반응형 구현

--- src/pages/Main/components/ListTitle/ListTitle.tsx
+++ src/pages/Main/components/ListTitle/ListTitle.tsx
@@ -1,5 +1,5 @@
 import { MouseEventHandler } from 'react';
-import { MoveRight } from 'lucide-react';
+import { ChevronRight, MoveRight } from 'lucide-react';
 
 type ListTitlePropsType = {
   title: string;
@@ -16,20 +16,25 @@ const ListTitle = (props: ListTitlePropsType) => {
   const { title, imgPath, description, onPageMove } = props;
 
   return (
-    <div className="flex justify-between items-center">
-      <div className="mt-20 mb-8">
-        <h2 className="flex gap-2 items-center">
-          <img src={imgPath} alt={title} className="w-9 h-9" />
-          <span className="text-2xl font-semibold">{title}</span>
-        </h2>
-        {description ? <p className=" mt-4 text-gray600 font-medium text-xl">{description}</p> : null}
+    <div className="flex flex-col justify-between mt-20 mb-8">
+      <h2 className="flex gap-2 items-center">
+        <img src={imgPath} alt={title} className="w-9 h-9" />
+        <span className="text-2xl font-semibold whitespace-nowrap">{title}</span>
+      </h2>
+      <div className="flex justify-between items-center mt-4">
+        {description ? <p className=" text-gray600 font-medium text-md sm:text-lg md:text-xl">{description}</p> : null}
+        {onPageMove ? (
+          <div className="flex items-center cursor-pointer hover:opacity-50" onClick={onPageMove}>
+            <div className="hidden md:flex md:items-center">
+              <span className=" text-gray400">더보기</span>
+              <MoveRight size={16} strokeWidth={0.5} className="ml-2" />
+            </div>
+            <div className="md:hidden mb-1  ">
+              <ChevronRight size={32} strokeWidth={1.6} />
+            </div>
+          </div>
+        ) : null}
       </div>
-      {onPageMove ? (
-        <div className="flex items-center cursor-pointer  self-end pb-8" onClick={onPageMove}>
-          <span className=" text-gray400">더보기</span>
-          <MoveRight size={16} strokeWidth={0.5} className="ml-2" />
-        </div>
-      ) : null}
     </div>
   );
 };
