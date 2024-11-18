683629d6136625a5ae557ce6b9d46333dcd0c094
SUMINMOON <sum95771@gmail.com>
2024-11-18 17:09:33 +0900

    feat: alt 및 이미지 사이즈 추가

--- src/pages/Main/components/ListTitle/ListTitle.tsx
+++ src/pages/Main/components/ListTitle/ListTitle.tsx
@@ -18,7 +18,7 @@ const ListTitle = (props: ListTitlePropsType) => {
   return (
     <div className="flex flex-col justify-between mt-20 mb-8">
       <h2 className="flex gap-2 items-center">
-        <img src={imgPath} alt={title} className="w-9 h-9" />
+        <img src={imgPath} alt={title} width={36} height={36} className="w-9 h-9" />
         <span className="text-2xl font-semibold whitespace-nowrap">{title}</span>
       </h2>
       <div className="flex justify-between items-center mt-4">
