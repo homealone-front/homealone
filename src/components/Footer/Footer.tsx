4313cd87969cb08241ae3c1347a50054cce479b9
SUMINMOON <sum95771@gmail.com>
2024-11-25 14:21:22 +0900

    feat: Footer에 개인정보처리방침 링크 추가 및 스타일 변경

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { PATH } from '@/constants/paths';
+
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="w-full h-fit bg-[#444444]">
+    <div className="w-full h-fit bg-[#444444] text-sm text-gray400">
       <div className="py-8 px-12 flex flex-col justify-center items-center gap-4">
         <img src="/icons/logo_svg_square_white.svg" alt="logo" width={80} height={80} className="w-20 h-20" />
-        <ul className="flex flex-wrap text-gray400 text-md gap-4">
+
+        <ul className="flex flex-wrap items-center  text-sm gap-4">
           <li className="li-after">AUTHOR: Clound 2기 Team-7</li>
           <li className="li-after">CONTACT: ax34554@gmail.com</li>
           <li className="li-after">ADDRESS: 서울특별시 강남구 선릉로 433, 신관 6층</li>
+          <div className="w-[1px] h-3 bg-gray400"></div>
+          <li className="li-after cursor-pointer hover:opacity-60" onClick={() => navigate(PATH.privacy)}>
+            개인정보처리방침
+          </li>
         </ul>
-        <p className="text-gray400">본 사이트는 포트폴리오용으로만 사용됩니다.</p>
+        <p>본 사이트는 포트폴리오용으로만 사용됩니다.</p>
         <div>
-          <address className="text-center not-italic text-gray400">ⓒ 2024 TEAM-7. ALL RIGHTS RESERVED.</address>
+          <address className="text-center not-italic ">ⓒ 2024 TEAM-7. ALL RIGHTS RESERVED.</address>
         </div>
       </div>
     </div>
